461dbd574e63ef65e9beae07b1d0319cdf0e1660
reooo <leohattori@icloud.com>
2021-08-27 09:49:20 +0900

    ファイル整理

--- GameProject/OpenGL/MainCameraObject.cpp
+++ GameProject/OpenGL/MainCameraObject.cpp
@@ -23,11 +23,7 @@ MainCameraObject::~MainCameraObject()
 */
 void MainCameraObject::UpdateGameObject(float _deltaTime)
 {
-	Vector3 pos = mOffsetPos + mPosition;
-	pos.x = mLerpObject.x + mOffsetPos.x;
-	pos.y = mLerpObject.y + mOffsetPos.y;
-	pos.z = mLerpObject.z + mOffsetPos.z;
-	mPosition = pos;/*Vector3::Lerp(position, pos, _deltaTime * 6.0f);*/
+	mPosition = mOffsetPos + mLerpObject;
 
 	SetPosition(mPosition);
 	Matrix4 view = Matrix4::CreateLookAt(mPosition, mLerpObject, Vector3::UnitY);
